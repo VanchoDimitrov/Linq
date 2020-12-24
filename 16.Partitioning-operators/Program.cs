2031bb1b6d4ec6704cdb270ec8f8c85ab596bb7b
Vancho Dimitrov <vancho.dimitrov@outlook.com>
2020-12-24 20:28:30 +0100

    update new

--- 16.Partitioning-operators/Program.cs
+++ 16.Partitioning-operators/Program.cs
@@ -13,6 +13,9 @@ namespace _16.Partitioning_operators
 
             var p = new Program();
             p.Skip();
+            //p.SkipWhile();
+            //p.Take();
+            //p.TakeWhile();
         }
 
 
@@ -23,6 +26,48 @@ namespace _16.Partitioning_operators
 
             // 2. Linq query
             IEnumerable<int> query1 = numbers.Skip(3);
+            // 3. Execute query
+            foreach (var i in query1)
+                Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+
+        public void SkipWhile()
+        {
+            // 1. Datasource
+            List<string> numbers = new List<string>() { "John", "Jason", "Jacky", "Monica" };
+
+            // 2. Linq query
+            IEnumerable<string> query1 = numbers.SkipWhile(x => x.Length <= 5);
+            // 3. Execute query
+            foreach (var i in query1)
+                Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+
+        public void Take()
+        {
+            // 1. Datasource
+            List<string> names = new List<string>() { "John", "Jason", "Jacky", "Monica" };
+
+            // 2. Linq query
+            IEnumerable<string> query1 = names.Take(3);
+            // 3. Execute query
+            foreach (var i in query1)
+                Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+
+        public void TakeWhile()
+        {
+            // 1. Datasource
+            List<string> names = new List<string>() { "John", "Jason", "Jacky", "Monica" };
+
+            // 2. Linq query
+            IEnumerable<string> query1 = names.TakeWhile(x => x.Length <= 5);
             // 3. Execute query
             foreach (var i in query1)
                 Console.WriteLine(i);
---
+++ 18.LINQ-Linq-query-Method-query/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.LINQ_Linq_query_Method_query
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Datasource
+            var persons = new List<Person>() {
+                new Person{ name="John", lastName="Doe"},
+                new Person{ name="Jason", lastName="Davidson"}
+            };
+
+            // 2. Linq query
+            var query1 = from p in persons
+                         where p.name == "John"
+                         select new
+                         {
+                             Name = p.name,
+                             LastName = p.lastName
+                         };
+            // 2.1 Meethod query
+            var query2 = persons.Where(y => y.name == "John").Select(x => new
+            {
+                Name = x.name,
+                LastName = x.lastName
+            });
+
+
+            // 3. Execute query
+            foreach (var i in query1)
+            {
+                Console.WriteLine(i.Name + " " + i.LastName);
+            }
+
+            Console.WriteLine();
+
+            foreach (var i in query2)
+            {
+                Console.WriteLine(i.Name + " " + i.LastName);
+            }
+
+            Console.ReadKey();
+        }
+
+        class Person
+        {
+            public string name { get; set; }
+            public string lastName { get; set; }
+        }
+    }
+}
